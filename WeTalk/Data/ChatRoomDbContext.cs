8eec4e19a7943ac2c204d5b866b58d114b1a9652
Shanoy <92985793+Cyberyawdie@users.noreply.github.com>
2022-03-01 20:12:06 -0500

    Changes to chat UI functionality. Added models for chat room and message

--- WeTalk/Data/ChatRoomDbContext.cs
+++ WeTalk/Data/ChatRoomDbContext.cs
@@ -5,6 +5,6 @@ namespace WeTalk.Data
     public class ChatRoomDbContext : DbContext
     {
         public  ChatRoomDbContext(DbContextOptions<ChatRoomDbContext> options) : base(options) { }
-        public DbSet<ChatRoom> ChatRooms => Set<ChatRoom>();
+        public DbSet<ChatRoomModel> ChatRooms => Set<ChatRoomModel>();
     }
 }
