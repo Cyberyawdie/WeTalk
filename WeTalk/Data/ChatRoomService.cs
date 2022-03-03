a7211854be93c43b978c8899979d0da757c370b7
Shanoy <92985793+Cyberyawdie@users.noreply.github.com>
2022-03-02 22:23:44 -0500

    update on chatroom page

---
+++ WeTalk/Data/ChatRoomService.cs
@@ -0,0 +1,15 @@
+﻿namespace WeTalk.Data
+{
+    public class ChatRoomService
+    {
+       
+        public static void GetChatRoom(ChatRoomDbContext db, ChatRoomModel chatRoom)
+        {
+            
+
+            db.ChatRooms.Add(chatRoom);
+            db.SaveChanges();
+            
+        }
+    }
+}
